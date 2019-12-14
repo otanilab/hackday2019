83fe62200787e119551c20881002a65d12d7237d
SoraY677 <SoraY677@outlook.com>
2019-12-14 21:02:33 +0900

    Trophyのリストデザイン作成途中ですが

---
+++ components/TrophyList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="trophy-list">
+    <ul class="trophy-list">
+      <li
+        v-for="trophyId in trophyIdList"
+        v-bind:key="trophyId.id"
+        class="trophy-item"
+      >
+        <img src="@/assets/trophy-fill.svg" />
+        <p class="tropy-name">{{ trophyId }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul.trophy-list {
+  overflow: hidden;
+  padding: 0;
+  width: 90%;
+  margin: auto;
+}
+.trophy-item {
+  list-style: none;
+  width: 33.3%;
+  float: left;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; /* 子要素をflexboxにより中央に配置する */
+}
+img {
+  width: 50%;
+  margin: 0 25% 0 25%;
+  overflow: hidden;
+  float: none;
+}
+.tropy-name {
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  props: { trophyIdList: { type: Array } }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,51 +5,44 @@
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
+/* Noto Sans */
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&display=swap');
 *,
 *:before,
 *:after {
+  font-family: 'Noto Sans JP', sans-serif;
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  color: #707070;
+  line-height: 1.5;
+}
+article,
+aside,
+footer,
+header,
+nav,
+section,
+main {
+  display: block;
+}
+img {
+  display: block;
+  flex: none;
+}
+a {
+  color: inherit;
   text-decoration: none;
-  padding: 10px 30px;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+ol,
+ul {
+  list-style: none;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+button {
+  background: none;
+  border: none;
 }
 </style>
