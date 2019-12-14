2c77aaf6845fe7d0cb0c7ac1976439d8f6e161fc
hiro <matsumurahiroshi@hiro.local>
2019-12-14 20:30:54 +0900

    MenuBar実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    <nuxt />
+    <div class="inner">
+      <!-- <nuxt /> -->
+    </div>
+    <menu-bar></menu-bar>
   </div>
 </template>
+
+<script>
+import MenuBar from '~/components/MenuBar.vue'
+
+export default {
+  components: {
+    MenuBar
+  }
+}
+</script>
 
 <style>
 /* Noto Sans */
@@ -14,6 +27,7 @@
   font-family: 'Noto Sans JP', sans-serif;
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
 }
 
 body {
@@ -51,5 +65,9 @@ ul {
 button {
   background: none;
   border: none;
+}
+
+.inner {
+  width: 95%;
 }
 </style>
